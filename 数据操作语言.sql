5b5af164a22bd1b2f07094f8ce74d12db183e417
LiuZijian <lzj@123>
2019-01-19 23:20:10 +0800

    add
    add

--- 数据操作语言.sql
+++ 数据操作语言.sql
@@ -22,3 +22,31 @@ SET
 	id = 14,
 	`name` = '刘涛',
 	phone = '135'
+
+	-- 修改多表记录
+
+UPDATE boys boy
+INNER JOIN beauty bea ON bea.boyfriend_id = boy.id
+SET bea.phone = "12344555"
+WHERE boy.id = 1
+
+
+UPDATE beauty 
+LEFT JOIN boys ON boys.id = beauty.boyfriend_id
+SET beauty.phone = "cn666"
+WHERE beauty.boyfriend_id is NULL
+
+-- 删除，有返回值，自增长从上个记录开始，支持事务回滚
+DELETE FROM beauty WHERE id = 27
+
+-- 删谁delete后面就写谁
+DELETE beauty
+FROM beauty INNER JOIN boys ON beauty.boyfriend_id = boys.id
+WHERE boys.id = 1
+
+DELETE beauty, boys
+FROM beauty INNER JOIN boys ON beauty.boyfriend_id = boys.id
+WHERE boys.id = 3
+
+-- 清空整个表,没有返回值，自增长从1开始，不支持事务回滚
+TRUNCATE TABLE beautyy
